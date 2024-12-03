0a8cd50a035c55420708910709af0a9d241b8d87
Venc03 <vegi.dani1@gmail.com>
2024-12-03 14:15:33 +0100

    fogyaszt from string to double

--- database/migrations/2024_11_19_124522_create_zsautos_table.php
+++ database/migrations/2024_11_19_124522_create_zsautos_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('marka');
             $table->string('tipus');
             $table->string('szin');
-            $table->string('fogyaszt');
+            $table->double('fogyaszt');
             $table->integer('ar');
             $table->timestamps();
         });
